2c88892e0942d99a31715d56121114ae8bc26ae2
TaeBbong <mok05289@korea.ac.kr>
2021-11-17 22:54:34 +0900

    [ADD] List Template

--- 03_TodoDjango/todo/urls.py
+++ 03_TodoDjango/todo/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.todo_list, name='todo_list'),
+]
--- 03_TodoDjango/todo/views.py
+++ 03_TodoDjango/todo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Todo
 
-# Create your views here.
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, 'todo/todo_list.html', {'todos': todos})
