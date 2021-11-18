bbe53143018abc9644dd2ab3a062988d77837f98
TaeBbong <mok05289@korea.ac.kr>
2021-11-18 21:40:50 +0900

    [ADD] todo_detail

--- 03_TodoDjango/todo/urls.py
+++ 03_TodoDjango/todo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.todo_list, name='todo_list'),
+    path('todo/<int:pk>', views.todo_detail, name='todo_detail'),
 ]
--- 03_TodoDjango/todo/views.py
+++ 03_TodoDjango/todo/views.py
@@ -5,3 +5,8 @@ from .models import Todo
 def todo_list(request):
     todos = Todo.objects.all()
     return render(request, 'todo/todo_list.html', {'todos': todos})
+
+
+def todo_detail(request, pk):
+    todo = Todo.objects.get(id=pk)
+    return render(request, 'todo/todo_detail.html', {'todo': todo})
