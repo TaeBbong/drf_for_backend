9e32c1d0053c1a0b1bc2402efd7ceef4f80aa451
TaeBbong <mok05289@korea.ac.kr>
2021-11-28 21:46:23 +0900

    [ADD] Todo Model

--- 05_DRFTodo/todo/models.py
+++ 05_DRFTodo/todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    important = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
