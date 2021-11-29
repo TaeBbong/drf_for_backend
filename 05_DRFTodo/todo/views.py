bf2e05deff7885da2f32652d5a8ea1898ccd4934
TaeBbong <mok05289@korea.ac.kr>
2021-11-29 20:19:10 +0900

    [ADD] Serializer, ViewSet

--- 05_DRFTodo/todo/views.py
+++ 05_DRFTodo/todo/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
