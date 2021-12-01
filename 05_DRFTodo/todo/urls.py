63524b613197b0db1ce78dede7312f90c7dcfa42
TaeBbong <mok05289@korea.ac.kr>
2021-12-01 21:23:54 +0900

    [ADD] APIView based CBV

--- 05_DRFTodo/todo/urls.py
+++ 05_DRFTodo/todo/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from rest_framework import routers
 
-from .views import TodoViewSet
+from .views import TodoAPIView, TodosAPIView, DoneTodoAPIView, DoneTodosAPIView
+# from .views import TodoViewSet
 
-router = routers.SimpleRouter()
-router.register('todo', TodoViewSet)
+# router = routers.SimpleRouter()
+# router.register('todo', TodoViewSet)
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
+
+urlpatterns = [
+    path('todo/', TodosAPIView.as_view()),
+    path('todo/<int:pk>/', TodoAPIView.as_view()),
+    path('done/', DoneTodosAPIView.as_view()),
+    path('done/<int:pk>/', DoneTodoAPIView.as_view()),
+]
--- 05_DRFTodo/todo/views.py
+++ 05_DRFTodo/todo/views.py
@@ -1,14 +1,65 @@
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from rest_framework import viewsets
+
 from .models import Todo
 from .serializers import TodoSerializer, TodoCreateSerializer
 
 
-class TodoViewSet(viewsets.ModelViewSet):
-    queryset = Todo.objects.all()
+class TodosAPIView(APIView):
+    def get(self, request):
+        todos = Todo.objects.filter(complete=False)
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    # serializer_class = TodoSerializer
+    def post(self, request):
+        serializer = TodoCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def get_serializer_class(self):
-        if self.action == 'create':
-            return TodoCreateSerializer
-        return TodoSerializer
+
+class TodoAPIView(APIView):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, id=pk)
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        todo = get_object_or_404(id=pk)
+        serializer = TodoCreateSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DoneTodosAPIView(APIView):
+    def get(self, request):
+        dones = Todo.objects.filter(complete=True)
+        serializer = TodoSerializer(dones, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class DoneTodoAPIView(APIView):
+    def get(self, request, pk):
+        done = get_object_or_404(Todo, id=pk)
+        done.complete = True
+        done.save()
+        serializer = TodoSerializer(done)
+        return Response(status=status.HTTP_200_OK)
+
+
+# class TodoViewSet(viewsets.ModelViewSet):
+#     queryset = Todo.objects.all()
+
+#     # serializer_class = TodoSerializer
+
+#     def get_serializer_class(self):
+#         if self.action == 'create':
+#             return TodoCreateSerializer
+#         return TodoSerializer
