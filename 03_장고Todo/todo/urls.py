2e7f7b97923952a2307eb604b6cc8644cca9cbfd
TaeBbong <mok05289@korea.ac.kr>
2021-12-02 21:07:45 +0900

    [MOD] URL 수정, Form Default로 변경

--- 03_장고Todo/todo/urls.py
+++ 03_장고Todo/todo/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.todo_list, name='todo_list'),
-    path('todo/<int:pk>', views.todo_detail, name='todo_detail'),
-    path('todo', views.todo_post, name='todo_post'),
-    path('done', views.done_list, name='done_list'),
-    path('todo/<int:pk>/done', views.todo_done, name='todo_done'),
-    path('todo/<int:pk>/edit', views.todo_edit, name='todo_edit'),
+    path('post/', views.todo_post, name='todo_post'),
+    path('<int:pk>/', views.todo_detail, name='todo_detail'),
+    path('<int:pk>/edit/', views.todo_edit, name='todo_edit'),
+    path('done/', views.done_list, name='done_list'),
+    path('done/<int:pk>/', views.todo_done, name='todo_done'),
 ]
