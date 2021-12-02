4e944efdfd480b50e5826f3c9ec9d5da71368107
TaeBbong <mok05289@korea.ac.kr>
2021-12-03 00:16:36 +0900

    [DEL] 불필요 필드 제거

--- 05_DRFTodo/todo/models.py
+++ 05_DRFTodo/todo/models.py
@@ -6,7 +6,6 @@ class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    datecompleted = models.DateTimeField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     important = models.BooleanField(default=False)
 
