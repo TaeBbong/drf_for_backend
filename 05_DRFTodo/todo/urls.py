6fe7367dc72d65727c51b99493c2a5df4fefdb39
TaeBbong <mok05289@korea.ac.kr>
2021-12-06 21:58:01 +0900

    [DEL] 불필요 코드 제거

--- 05_DRFTodo/todo/urls.py
+++ 05_DRFTodo/todo/urls.py
@@ -1,13 +1,6 @@
 from django.urls import path
-from rest_framework import routers
 
 from .views import TodoAPIView, TodosAPIView, DoneTodoAPIView, DoneTodosAPIView
-# from .views import TodoViewSet
-
-# router = routers.SimpleRouter()
-# router.register('todo', TodoViewSet)
-
-# urlpatterns = router.urls
 
 urlpatterns = [
     path('todo/', TodosAPIView.as_view()),
